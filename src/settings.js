8d824c14ac28d7f7a5e595b10b0313fa38fac096
jacmac2812 <simonsen.jac@icloud.com>
2021-01-16 16:29:05 +0100

    add

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
-//export default URL = "https://www.legepladss.dk/tomcat/ca3";
+//export default URL = "https://www.jssimsen.com/tomcat/eksamen";
 
 export default URL = "http://localhost:8080/jpareststarter";
